067162c4b45153aa3e581af76e91fc6c644f77ec
SharifTanvir <mislam151243@bscse.uiu.ac.bd>
2022-06-29 16:19:01 +0600

    Lesson 10 done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 
-import ClockList from './components/ClockList';
+import From from './components/Form';
 //yfgu
 function App() {
-    const quantities = [1, 2, 3];
     return (
         <div>
-            <ClockList quantities={quantities} />
+            <From />
         </div>
     );
 }
